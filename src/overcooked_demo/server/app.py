aa130bfcae4c15e3d6351dfaf28277c5e073d0b9
mosmith3asu <62581907+mosmith3asu@users.noreply.github.com>
2024-05-08 10:36:38 -0700

    Started webserver implementation

--- src/overcooked_demo/server/app.py
+++ src/overcooked_demo/server/app.py
@@ -659,13 +659,9 @@ def play_game(game: OvercookedGame, fps=6):
 
 
 if __name__ == "__main__":
-
-    # HOST = "0.0.0.0"; PORT = 80 # for docker implementation
-    HOST = "127.0.0.1";   PORT = 8000  # for direct flask app deployment
-
     # Dynamically parse host and port from environment variables (set by docker build)
-    host = os.getenv("HOST", HOST)
-    port = int(os.getenv("PORT", PORT))
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 80))
 
     # Attach exit handler to ensure graceful shutdown
     atexit.register(on_exit)
--- src/overcooked_demo/server/game.py
+++ src/overcooked_demo/server/game.py
@@ -11,10 +11,10 @@ import ray
 from utils import DOCKER_VOLUME, create_dirs
 
 from human_aware_rl.rllib.rllib import load_agent
-from risky_overcooked_py.mdp.actions import Action, Direction
-from risky_overcooked_py.mdp.overcooked_env import OvercookedEnv
-from risky_overcooked_py.mdp.overcooked_mdp import OvercookedGridworld
-from risky_overcooked_py.planning.planners import (
+from overcooked_ai_py.mdp.actions import Action, Direction
+from overcooked_ai_py.mdp.overcooked_env import OvercookedEnv
+from overcooked_ai_py.mdp.overcooked_mdp import OvercookedGridworld
+from overcooked_ai_py.planning.planners import (
     NO_COUNTERS_PARAMS,
     MotionPlanner,
 )

c7e081f34b0aa3cfb41f0f5c4720c0bae2084871
mosmith3asu <62581907+mosmith3asu@users.noreply.github.com>
2024-07-29 13:00:58 -0700

    Testing random chance start state schedule

--- src/risky_overcooked_rl/utils/trainer.py
+++ src/risky_overcooked_rl/utils/trainer.py
@@ -333,22 +333,21 @@ def main():
     ###############################
 
     # Top Left
-    # config['ITERATIONS'] = 30_000
-    # config['LAYOUT'] = "risky_coordination_ring"
-    # config['replay_memory_size'] = 30_000
-    # config['epsilon_sched'] = [1.0, 0.15, 10_000]
-    # config['rshape_sched'] = [1, 0, 10_000]
-    # config['rationality_sched'] = [5.0, 5.0, 10_000]
-    # config['lr_sched'] = [1e-2, 1e-4, 3_000]
-    # config['perc_random_start'] = 0.9
+    config['ITERATIONS'] = 30_000
+    config['LAYOUT'] = "risky_coordination_ring"
+    config['replay_memory_size'] = 30_000
+    config['epsilon_sched'] = [1.0, 0.1, 15_000]
+    config['rshape_sched'] = [1, 0, 10_000]
+    config['rationality_sched'] = [5.0, 5.0, 10_000]
+    config['lr_sched'] = [1e-2, 1e-4, 3_000]
     config["rand_start_sched"]= [1.0, 0.75, 10_000]  # percentage of ITERATIONS with random start states
-    # config['tau'] = 0.01
-    # config['num_hidden_layers'] = 5
-    # config['size_hidden_layers'] = 256
-    # config['gamma'] = 0.95
-    # config['p_slip'] = 0.25
-    # config['note'] = 'medium risk + random chance start'
-    # Trainer(SelfPlay_QRE_OSA, config).run()
+    config['tau'] = 0.01
+    config['num_hidden_layers'] = 5
+    config['size_hidden_layers'] = 256
+    config['gamma'] = 0.95
+    config['p_slip'] = 0.25
+    config['note'] = 'medium risk + random chance start'
+    Trainer(SelfPlay_QRE_OSA, config).run()
 
 
 
@@ -425,32 +424,32 @@ def main():
     # config['epsilon_sched'] = [1.0, 0.15, 10_000]
     # config['rshape_sched'] = [1, 0, 10_000]
     # config['rationality_sched'] = [5.0, 5.0, 10_000]
-    # config['lr_sched'] = [1e-2, 1e-5, 4_000]
-    # config['perc_random_start'] = 0.9
+    # config['lr_sched'] = [1e-2, 1e-4, 4_000]
+    # config["rand_start_sched"] = [0.0, 0.00, 15_000]  # config['perc_random_start'] = 0.9
     # config['tau'] = 0.01
     # config['num_hidden_layers'] = 5
     # config['size_hidden_layers'] = 256
     # config['gamma'] = 0.97
-    # config['p_slip'] = 0.1
-    # config['note'] = 'minimal risk + chance start + increased gamma + lower LR'
+    # config['p_slip'] = 0.99
+    # config['note'] = 'max risk + (test handoff)'
     # Trainer(SelfPlay_QRE_OSA, config).run()
 
     # bottom Right
-    config['LAYOUT'] = "risky_coordination_ring"
-    config['ITERATIONS'] = 30_000
-    config['replay_memory_size'] = 30_000
-    config['epsilon_sched'] = [1.0, 0.15, 10_000]
-    config['rshape_sched'] = [1, 0, 10_000]
-    config['rationality_sched'] = [5.0, 5.0, 10_000]
-    config['lr_sched'] = [1e-2, 1e-4, 3_000]
-    config["rand_start_sched"] = [1.0, 0.1, 10_000]  # config['perc_random_start'] = 0.9
-    config['tau'] = 0.01
-    config['num_hidden_layers'] = 5
-    config['size_hidden_layers'] = 256
-    config['gamma'] = 0.97
-    config['p_slip'] = 0.1
-    config['note'] = 'minimal risk + chance start + increased gamma + lower LR'
-    Trainer(SelfPlay_QRE_OSA, config).run()
+    # config['LAYOUT'] = "risky_coordination_ring"
+    # config['ITERATIONS'] = 30_000
+    # config['replay_memory_size'] = 30_000
+    # config['epsilon_sched'] = [1.0, 0.15, 10_000]
+    # config['rshape_sched'] = [1, 0, 10_000]
+    # config['rationality_sched'] = [5.0, 5.0, 10_000]
+    # config['lr_sched'] = [1e-2, 1e-4, 3_000]
+    # config["rand_start_sched"] = [1.5, 0.05, 15_000]  # config['perc_random_start'] = 0.9
+    # config['tau'] = 0.01
+    # config['num_hidden_layers'] = 5
+    # config['size_hidden_layers'] = 256
+    # config['gamma'] = 0.95
+    # config['p_slip'] = 0.1
+    # config['note'] = 'minimal risk + chance start'
+    # Trainer(SelfPlay_QRE_OSA, config).run()
     # config['LAYOUT'] = "risky_coordination_ring"
     # config['ITERATIONS'] = 30_000
     # config['replay_memory_size'] = 30_000

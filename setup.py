58103805731652e966fcca6b3a239629ecfca42d
mosmith3asu <62581907+mosmith3asu@users.noreply.github.com>
2025-09-02 19:33:43 -0700

    updated webserver (begin docker testing)

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "flask==2.1.3",
         "flask-socketi==4.3.1",
         "numba==0.61.2"
+        "torch==2.3.1+cu118"
     ],
     # install_requires=[
     #     "dill",
@@ -55,20 +56,20 @@ setup(
     #     "numba=0.61.2"
     # ],
     # removed overlapping dependencies
-    extras_require={
-        "harl": [
-            "wandb",
-            "GitPython",
-            "memory_profiler",
-            "sacred",
-            "pymongo",
-            "matplotlib==3.8.3",
-            "requests",
-            # "seaborn==0.9.0",
-            # "ray[rllib]>=2.5.0",
-            "protobuf",
-            "torch==2.3.1+cu118"
-            # "tensorflow>=2.14.0",
-        ]
-    },
+    # extras_require={
+    #     "harl": [
+    #         "wandb",
+    #         "GitPython",
+    #         "memory_profiler",
+    #         "sacred",
+    #         "pymongo",
+    #         "matplotlib==3.8.3",
+    #         "requests",
+    #         # "seaborn==0.9.0",
+    #         # "ray[rllib]>=2.5.0",
+    #         "protobuf",
+    #         "torch==2.3.1+cu118"
+    #         # "tensorflow>=2.14.0",
+    #     ]
+    # },
  )
